fdc199ccd826db67487a0279972fd2d17505036a
Aleksandra Mętrak <jurek.aleksandra@gmail.com>
2020-05-17 12:54:11 +0200

    minority name animation: hiding

--- src/components/minority-name.js
+++ src/components/minority-name.js
@@ -1,20 +1,73 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "gatsby";
-// import "typeface-ibm-plex-sans";
+import { gsap } from "gsap";
 
-const Name = styled.p`
+const FullName = styled.p`
+  display: flex;
+  justify-content: center;
   margin: 1.2em 0;
   text-align: center;
-  //   font-family: "IBM Plex Sans", sans-serif;
   color: #1a1a1a;
 `;
 
+const HalfName = styled.span`
+  position: relative;
+  display: flex;
+  overflow: hidden;
+`;
+
+const HalfNameContent = styled.span`
+  display: inline-block;
+`;
+
 const MinorityName = ({ name }) => {
-  //   console.log(name);
+  const firstHalf = name.slice(0, Math.floor(name.length / 2));
+  const lastHalf = name.slice(Math.floor(name.length / 2));
+
+  const firstHalfRef = useRef(null);
+  const firstHalfTween = useRef(null);
+  const lastHalfRef = useRef(null);
+  const lastHalfTween = useRef(null);
+
+  // const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
+
+  useEffect(() => {
+    firstHalfTween.current = gsap.to(firstHalfRef.current, {
+      duration: 0.5,
+      x: "+=100%",
+      paused: true,
+    });
+    lastHalfTween.current = gsap.to(lastHalfRef.current, {
+      duration: 0.5,
+      x: "-=100%",
+      paused: true,
+    });
+  }, []);
+
+  const onMouseEnterHandler = () => {
+    firstHalfTween.current.play();
+    lastHalfTween.current.play();
+  };
+  const onMouseLeaveHandler = () => {
+    firstHalfTween.current.reverse();
+    lastHalfTween.current.reverse();
+  };
+
   return (
-    <Link to="/">
-      <Name>{name}</Name>
+    <Link
+      to="/"
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+    >
+      <FullName>
+        <HalfName>
+          <HalfNameContent ref={firstHalfRef}>{firstHalf}</HalfNameContent>
+        </HalfName>
+        <HalfName>
+          <HalfNameContent ref={lastHalfRef}>{lastHalf}</HalfNameContent>
+        </HalfName>
+      </FullName>
     </Link>
   );
 };
