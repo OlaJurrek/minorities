5583aae84d8dd6ca1cead354e78ba627982a61fd
Aleksandra Mętrak <aleksandra.metrak@gmail.com>
2021-03-02 11:38:23 +0100

    add netlify cms config - one language

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-web-font-loader",
