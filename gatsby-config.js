ba30ac9d4214ff70306cd5584e515aff133f1974
Aleksandra Mętrak <aleksandra.metrak@gmail.com>
2021-03-03 12:25:14 +0100

    source minorities data - part 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,21 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `minorities-pages`,
+        path: `${__dirname}/src/content/minorities/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/content/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         custom: {
--- src/pages/minority.js
+++ src/pages/minority.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import Headline from '../components/atoms/headline';
-import Paragraph from '../components/atoms/paragraph';
-import LanguageNav from '../components/molecules/language-nav';
+import Headline from "../components/atoms/headline";
+import Paragraph from "../components/atoms/paragraph";
+import LanguageNav from "../components/molecules/language-nav";
 
-const MinorityPage = () => (
-  <Layout>
-    <LanguageNav/>
-    <Headline text="Hello from minority page! This is headline"/>
-    <Paragraph text="Pierwsze duże migracje Czechów na tereny Polski związane były z czynnikami religijnymi. Ich historię otwiera rok 1548, gdy Ferdynand I nakazał wszystkim poddanym należącym do husyckiej Jednoty Braterskiej wstąpienie do Kościoła katolickiego albo opuszczenie kraju[5]. Osiedlenie się w Wielkopolsce braci czeskich, którzy jako ruch religijno-społeczny wyłonili się z husytyzmu, było niezwykle istotne dla rozwoju polskiej reformacji. Inspiracje czeskie odegrały zwłaszcza rolę w początkowej fazie formowania się polskiej literatury reformacyjnej w I połowie XVI wieku[6]." />
-  </Layout>
-);
+const MinorityPage = ({ data }) => {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
+  console.log(frontmatter);
+  console.log(html);
+  return (
+    <Layout>
+      <LanguageNav />
+      <Headline text="Hello from minority page! This is headline" />
+      <Paragraph text="Pierwsze duże migracje Czechów na tereny Polski związane były z czynnikami religijnymi. Ich historię otwiera rok 1548, gdy Ferdynand I nakazał wszystkim poddanym należącym do husyckiej Jednoty Braterskiej wstąpienie do Kościoła katolickiego albo opuszczenie kraju[5]. Osiedlenie się w Wielkopolsce braci czeskich, którzy jako ruch religijno-społeczny wyłonili się z husytyzmu, było niezwykle istotne dla rozwoju polskiej reformacji. Inspiracje czeskie odegrały zwłaszcza rolę w początkowej fazie formowania się polskiej literatury reformacyjnej w I połowie XVI wieku[6]." />
+    </Layout>
+  );
+};
+
+// export const pageQuery = graphql`
+//   query($slug: String!) {
+//     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+//       html
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         slug
+//         title
+//       }
+//     }
+//   }
+// `;
 
 export default MinorityPage;
